d61cb2238c60d6ec5689244c49b4a2cc8be0b032
Serhii Chernetskiy <sergchernetskiy@gmail.com>
2023-02-25 12:33:52 +0200

    add notification

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import { Notify } from "notiflix/build/notiflix-notify-aio";
 import "./css/style.css";
 import { formRef, listRef } from "./refs";
 import { createMarkup } from "./markup";
@@ -7,6 +8,9 @@ import { saveMessage, load, save } from "./service";
 init();
 
 formRef.addEventListener("submit", onFormSubmit);
+listRef.addEventListener("click", onDeleteBtnClick);
+listRef.addEventListener("click", onTaskChecked);
+listRef.addEventListener("click", onEditBtnClick);
 
 function onFormSubmit(e) {
   e.preventDefault();
@@ -16,6 +20,10 @@ function onFormSubmit(e) {
   } = formRef.elements;
 
   const inputValue = value.trim();
+
+  if (inputValue === "") {
+    Notify.failure("You can not add an empty task!");
+  }
 
   if (!inputValue) {
     return;
@@ -35,4 +43,36 @@ function init() {
   const data = load();
 
   addMarkup(createMarkup(data));
+}
+
+function onDeleteBtnClick(e) {
+  if (e.target.classList.contains("button__task")) {
+    const liRef = e.target.closest(".item");
+    const dataAttr = liRef.dataset.id;
+    liRef.remove();
+    const data = load();
+    const filteredData = data.filter(({ id }) => id !== Number(dataAttr));
+    save(filteredData);
+  }
+}
+
+function onTaskChecked(e) {
+  if (e.target.nodeName === "P") {
+    e.target.classList.add("checked");
+  }
+}
+
+function onEditBtnClick(e) {
+  const field = e.target.closest(".item");
+  const input = field.firstElementChild;
+
+  if (e.target.innerText.toLowerCase() === "edit") {
+    input.removeAttribute("readonly");
+    input.focus();
+    input.selectionStart = input.value.length;
+    e.target.innerText = "Save";
+  } else {
+    input.setAttribute("readonly", "readonly");
+    e.target.innerText = "Edit";
+  }
 }
