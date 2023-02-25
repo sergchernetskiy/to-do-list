3bc2f4cbf821f23fa17a265d9f5ee92eed0e7877
Serhii Chernetskiy <sergchernetskiy@gmail.com>
2023-02-25 11:47:18 +0200

    initial files

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/main.js"),
+  output: { path: path.resolve(__dirname, "build"), filename: "main.js" },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, "./src/template/index.html"),
+      filename: "index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  devServer: {
+    port: 8080,
+    static: {
+      directory: path.join(__dirname, "src"),
+    },
+  },
+};
